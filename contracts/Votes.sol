65443122350225aea8972d5cbf7e30c9c5b74c21
Gaoyagi <aoyagigeorge@gmail.com>
2020-10-04 04:31:53 -0700

    first commit

---
+++ contracts/Votes.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.7.0;
+
+contract Vote{
+    function transferFrom(address _from, address _to, uint256 _tokenId) public{
+
+    }
+
+    function  balanceOf(address _owner) public {
+
+    }
+    
+
+}
